2db687a21970370aed3999f900ec291242a9d979
小天 <xjt520@gmail.com>
2019-09-16 15:10:46 +0800

    addTwoNumbers

---
+++ src/com/leet/code/addTwoNumbers/ListNode.java
@@ -0,0 +1,14 @@
+package com.leet.code.addTwoNumbers;
+
+public class ListNode {
+
+    //当前节点值
+    int val;
+
+    //下个节点
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/leet/code/addTwoNumbers/Solution.java
@@ -0,0 +1,27 @@
+package com.leet.code.addTwoNumbers;
+
+class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+            int sun = x + y + carry;
+            //取出进位
+            carry = sun / 10;
+            cur.next = new ListNode(sun % 10);
+            cur = cur.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+}
